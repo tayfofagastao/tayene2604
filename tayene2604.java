1f1e771fb8a50e30d0877b27c5601c4f0b9b2326
tayfofagastao <161323985+tayfofagastao@users.noreply.github.com>
2024-04-26 11:44:21 -0300

    Add files via upload

---
+++ tayene2604.java
@@ -0,0 +1,31 @@
+package candicional;
+
+import java.util.Scanner;
+
+public class tayene2604 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sal_carlos, sal_joao;
+		int meses_numericos;
+		while (meses_numericos <-0);
+		while (sal_joao <- sal_carlos /3) {
+			
+			sal_joao < sal_carlos
+			System.out.println("Meses para o salário do joão ser maior do que o do carlos");
+			
+	        float sal_carlos <- sal_carlos + ("sal_carlos * / 100");
+	        float 
+
+		}
+		
+			
+		Scanner scanner = new Scanner(System.in);
+
+		
+		
+		
+
+	}
+
+}
